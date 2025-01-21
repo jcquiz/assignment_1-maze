00f48a8c587d1f72b85c10def70aed3bda61ad59
JC Quizon <quizoj1@mcmaster.ca>
2025-01-20 19:49:27 -0500

    introducing depency, logging and creating maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,7 +1,6 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,26 +10,55 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
+            logger.info("**** Reading the maze from file " + args[0]);
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
-                    }
+            
+            
+            
+            // puts the maze into a 2d array of characters
+            char matrix[][];
+            String line = reader.readLine();
+            matrix = new char[line.length()][line.length()];
+            int row = 0;
+            do 
+            {
+                for (int idx = 0; idx < line.length(); idx++) 
+                {
+                        if (line.charAt(idx) == '#') 
+                        {
+                            matrix[row][idx] = '#';
+                        } 
+                        else if (line.charAt(idx) == ' ') 
+                        {
+                            matrix[row][idx] = ' ';
+                        }  
                 }
-                System.out.print(System.lineSeparator());
+                row++;
+            }while ((line = reader.readLine()) != null);
+ 
+
+
+
+            // prints the maze
+            for(int row1 = 0 ; row1 < matrix.length; row1++)
+            {
+                for(int col = 0; col < matrix[0].length; col++)
+                {
+                    System.out.print(matrix[row1][col]);
+                }
+                System.out.println();
             }
+
+
+
+
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.info("**** Computing path");
+        logger.info("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
 }
